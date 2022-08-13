056e9ab36c7d948a075eb539e34348f6c8809375
Artem Aniskin <artemaistrike@gmail.com>
2022-08-13 20:16:13 +0300

    change colors

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ export default function App() {
     return (
         <Routes>
             <Route path="/" element={<Layout />}>
-                ''
                 <Route path="" element={<Home />} />
                 <Route path="cart" element={<Cart />} />
                 <Route path="product/:id" element={<FullProduct />} />
